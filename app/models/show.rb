4fd88836c0c2c22ff49fe71d2f7b8b4ee3e3d5de
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-16 18:32:38 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -26,7 +26,7 @@ class Show < ActiveRecord::Base
     Show.where("rating > ?",5)
   end
 
-  def shows_by_alphabetical_order
+  def self.shows_by_alphabetical_order
     Show.order(:name)
   end
 end
