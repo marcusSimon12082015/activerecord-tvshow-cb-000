a1ef92f027b849f637de04b888e735e998b30fe2
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-16 18:25:48 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,7 @@ class Show < ActiveRecord::Base
     Show.maximum('rating')
   end
   def self.most_popular_show
-    Show.where("rating = ?",Show.maximum('rating'))
+    show = Show.where("rating = ?",Show.maximum('rating'))
+    show
   end
 end
